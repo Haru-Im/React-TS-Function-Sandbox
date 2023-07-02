0d52f5d43d77c4f056a5924d33248d6aa80f625b
Haru Im <chalk323@naver.com>
2023-07-02 19:16:09 +0900

    Add Recoil and implement RecoilProvider

--- searchbar/src/App.tsx
+++ searchbar/src/App.tsx
@@ -1,12 +1,13 @@
 import { Reset } from "styled-reset";
 import "./App.css";
 import { SearchContainer } from "./pages/searchbar/container";
+import { RecoilProvider } from "./provider/recoil";
 
 export const App: React.FC = () => {
   return (
-    <>
+    <RecoilProvider>
       <Reset />
       <SearchContainer />
-    </>
+    </RecoilProvider>
   );
 };
